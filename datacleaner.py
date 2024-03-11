49b0aca9d77a02c4bd26e8ef52f8f51b731d42d1
Peter537 <pean@outlook.dk>
2024-03-11 14:40:02 +0100

    Update datacleaner.py

--- datacleaner.py
+++ datacleaner.py
@@ -37,4 +37,47 @@ def load_data(path):
 
     return df, dfNumeric, dfNoOutliers
 
+def load_country_gdp_data(path):
+    df = pd.read_csv(path)
+
+    print("--- Check for data types ---")
+    print(df.info())
+
+    print("--- Check for shape ---")
+    print(df.shape)
+
+    df = df[['Country Name', 'Year', 'GDP', 'Population', 'GDP Per Capita']]
+    df = df.rename(columns={'Country Name': 'country', 'Year': 'year', 'GDP': 'gdp', 'Population': 'population', 'GDP Per Capita': 'gdp_per_capita'})
+
+    print("--- Check for missing values ---")
+    print(df.isnull().sum())
+
+    print("--- Check for NaN values ---")
+    print(df.isna().sum())
+
+    print("--- Check for data types ---")
+    print(df.info())
+
+    print("--- Check for shape ---")
+    print(df.shape)
+
+    # print cols where gdp is null
+    df.dropna(subset=['gdp'], inplace=True)
+
+    print("--- Check for missing values ---")
+    print(df.isnull().sum())
+
+    print("--- Check for shape ---")
+    print(df.shape)
+
+    print(df['country'].unique())
+
+    return df
+
 df, dfNumeric, dfNoOutliers = load_data('data/data_science_salaries.csv')
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+dfGDP = load_country_gdp_data("data/country_gdp_data.csv")
+print(df["company_location"].unique())
