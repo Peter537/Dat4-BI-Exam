f9db761b737cfe29179e6741acaf4dfbd230dc94
MagKon <magnus.kongstad@gmail.com>
2024-03-12 14:39:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ st.markdown(banner, unsafe_allow_html=True)
 
 try:
     from datacleaner import load_data, load_country_gdp_data, combined_df, get_numeric_df
-    
+
     if glob.glob("data/data_science_salaries.*"):
         st.session_state['df'] = load_data('data/data_science_salaries.csv')
     else:
@@ -51,5 +51,5 @@ try:
     st.session_state['dfCombined'] = combined_df()
     st.session_state['dfNumeric'] = get_numeric_df(st.session_state['dfCombined'])
 except:
-    st.error("Error loading data. Please make sure the data files are in the data folder and try again.")
+    st.error("Error loading data. Please make sure the data files are in the data folder and try again. \n\n The data folder should contain: \n\n data_science_salaries.csv \n\n country_gdp_data.csv")
     st.error("If these errors persist, feel free to contact the developers at cph-mk797@cphbusiness.dk")
