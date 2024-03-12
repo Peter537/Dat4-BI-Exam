93996e7a973a3d7868cfff56f3db93c31506cf14
MagKon <magnus.kongstad@gmail.com>
2024-03-12 11:08:39 +0100

    Update Prediction With ML.py

--- pages/Prediction With ML.py
+++ pages/Prediction With ML.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from sklearn.cluster import KMeans
 
 st.set_page_config(
     page_title="Prediction With ML",
@@ -7,20 +8,46 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-df = st.session_state['df'].copy()
-df = df.iloc[:1]
-st.data_editor(df)
+st.write("")
 
 tab1, tab2, tab3, tab4 = st.tabs(["Regression", "Classification", "Clustering", "About"])
 
 with tab1:
+    df = st.session_state["df"]
     st.write("Regression")
 
 with tab2:
+    df = st.session_state["df"]
     st.write("Classification")
 
 with tab3:
-    st.write("Clustering")
+    st.title("Clustering")
+
+    df = st.session_state['dfNumeric'].drop(['gdp_per_capita'], axis=1)
+
+    X = df.copy()
+    num_clusters = 3
+    kmeans = KMeans(init='k-means++', n_clusters=num_clusters, n_init=10, random_state=42)
+    kmeans.fit(X)
+    y = kmeans.predict(X)
+
+    if st.button("Randomize data"):
+        df = df.sample(frac=1).reset_index(drop=True)
+        st.write("Data has been randomized")
+    
+    df = st.data_editor(df.iloc[:1])
+
+    if (st.button("Predict cluster")):
+        st.write("The predicted cluster for the selected data is: " + kmeans.predict(df)[0].__str__()) # This does not seem to work?
+
+    st.title("KMeans Clustering Analysis")
+
+    from yellowbrick.cluster import SilhouetteVisualizer
+    visualizer = SilhouetteVisualizer(kmeans, colors='yellowbrick')
+    visualizer.fit(X)
+    fig = visualizer._fig
+    st.pyplot(fig)
+    st.write("The silhouette score of the model is: " + round(visualizer.silhouette_score_*100, 2).__str__() + "%")
 
 with tab4:
     st.write("About")
