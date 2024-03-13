7570ebe97aabea9206d6fa18f098a70d088dad37
Peter537 <52176852+Peter537@users.noreply.github.com>
2024-03-13 09:53:15 +0100

    Update Prediction With ML.py

--- pages/Prediction With ML.py
+++ pages/Prediction With ML.py
@@ -161,37 +161,37 @@ with tab2:
 
     def createNewRow(job_title, experience_level, company_location, work_model, work_year, salaryUSD, employment_type, company_size):
 
-            inputs = {}
+        inputs = {}
 
-            # Directly assign values for columns without prefixes
-            direct_columns = ['work_year', 'salary_in_usd']
-            direct_values = [work_year, salaryUSD]
+        # Directly assign values for columns without prefixes
+        direct_columns = ['work_year', 'salary_in_usd']
+        direct_values = [work_year, salaryUSD]
 
-            for direct_column, direct_value in zip(direct_columns, direct_values):
-                inputs[direct_column] = direct_value
+        for direct_column, direct_value in zip(direct_columns, direct_values):
+            inputs[direct_column] = direct_value
 
-            prefixes = ['job_title_', 'experience_level_', 'company_location_', 'work_models_', 'employment_type_', 'company_size_']
+        prefixes = ['job_title_', 'experience_level_', 'company_location_', 'work_models_', 'employment_type_', 'company_size_']
 
-            # Iterate through prefixes and input values to create the input dictionary
-            for prefix, value in zip(prefixes, [job_title, experience_level, company_location, work_model, employment_type, company_size]):
-                column_name = f"{prefix}{value}"
-                if column_name in dfCluster.columns:
-                    inputs[column_name] = 1
-                    # Set all other columns with the same prefix to 0
-                    for col in dfCluster.columns:
-                        if col.startswith(prefix) and col != column_name:
-                            inputs[col] = 0
+        # Iterate through prefixes and input values to create the input dictionary
+        for prefix, value in zip(prefixes, [job_title, experience_level, company_location, work_model, employment_type, company_size]):
+            column_name = f"{prefix}{value}"
+            if column_name in dfCluster.columns:
+                inputs[column_name] = 1
+                # Set all other columns with the same prefix to 0
+                for col in dfCluster.columns:
+                    if col.startswith(prefix) and col != column_name:
+                        inputs[col] = 0
 
-            # Convert the dictionary to a Series and then to a DataFrame
-            input_row = pd.Series(inputs)
-            input_row = pd.DataFrame([input_row])
+        # Convert the dictionary to a Series and then to a DataFrame
+        input_row = pd.Series(inputs)
+        input_row = pd.DataFrame([input_row])
 
-            # Ensure the input_row has the same columns as dfCluster
-            input_row = input_row.reindex(columns=dfCluster.columns, fill_value=0)
+        # Ensure the input_row has the same columns as dfCluster
+        input_row = input_row.reindex(columns=dfCluster.columns, fill_value=0)
 
-            st.write(input_row)
+        st.write(input_row)
 
-            return input_row
+        return input_row
 
 
 
