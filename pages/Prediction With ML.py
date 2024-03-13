8a961ccd724ab314d71b6a568924869b1409fd8d
Peter537 <52176852+Peter537@users.noreply.github.com>
2024-03-13 09:38:53 +0100

    added regressor

--- pages/Prediction With ML.py
+++ pages/Prediction With ML.py
@@ -4,10 +4,11 @@ import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, root_mean_squared_error
+from sklearn.metrics import mean_squared_error, root_mean_squared_error, r2_score
 import pickle
 import glob
-import datacleaner 
+from sklearn.ensemble import RandomForestRegressor
+import newRowGenerator as ng
 
 st.set_page_config(
     page_title="Prediction With ML",
@@ -26,6 +27,7 @@ classification = None
 kmeans = None
 dfCluster = st.session_state['dfNumeric'].drop(['gdp_per_capita'], axis=1)
 dfClassification = st.session_state['dfNumeric'].copy()
+dfNum = st.session_state['dfNumeric'].copy()
 
 try:
     st.warning("If this is the first time you are running this app, it may take a while to load the models as they will be trained. Please be patient.")
@@ -34,7 +36,26 @@ try:
         regression = pickle.load(open("regression.pkl", "rb"))
         pass
     else:
-        # Add regression here
+        # Splitting the data into features (X) and target variable (y)
+        X = dfNum.drop('salary_in_usd', axis=1)
+        y = dfNum['salary_in_usd']
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=83)
+
+        rf_regressor = RandomForestRegressor(n_estimators=50, random_state=116)
+
+        rf_regressor.fit(X_train, y_train)
+
+        y_pred = rf_regressor.predict(X_test)
+
+        mse = mean_squared_error(y_test, y_pred)
+
+        rmse = np.sqrt(mse)
+
+        r2 = r2_score(y_test, y_pred)
+        r2 = r2*100
+        r2 = round(r2, 2)
+
         pass
 
     if glob.glob("cluster.pkl") and glob.glob("data/cluster.csv"):
@@ -78,9 +99,29 @@ tab1, tab2, tab3, tab4 = st.tabs(["Regression", "Clustering", "Classification", 
 
 with tab1:
     df = st.session_state["df"]
-    st.write("Regression")
+    st.title("Regression")
+    #MSE telling us that a considerable amount of units discreptancy between predicted and actual values
+    st.write(f'Mean Squared Error (MSE): {mse:.2f}')
+    #RMSE telling us that the salary deviates around 62900 USD from the actual values
+    st.write(f'Root Mean Squared Error (RMSE): {rmse:.2f}')
+    #R2 is basically our accuracy score.
+    st.write(f'R-squared (R2): {r2}')
 
     st.title("Regression Analysis")
+    # Dropdown for Job Title
+    job_title_input = st.selectbox("Job Titles", df['job_title'].unique() )
+    # Dropdown for Experience Level
+    experience_level_input = st.selectbox("Experience Levels", df['experience_level'].unique()) # Make the order alphabetical
+    # Dropdown for Company Location
+    company_location_input = st.selectbox("Company Locations", df['company_location'].unique())
+
+    if (st.button("Predict Salary")):
+#        new_data_point = ng.create_input_row('Marketing Data Scientist', 'Senior-level', 'United States', dfNum.columns)
+        new_data_point = ng.create_input_row(job_title_input, experience_level_input, company_location_input, dfNum.columns)
+
+        predicted_salary = rf_regressor.predict(new_data_point)[0]
+
+        st.write(f'Predicted Salary: {predicted_salary} USD')
 
 with tab2:
     st.title("Clustering")
