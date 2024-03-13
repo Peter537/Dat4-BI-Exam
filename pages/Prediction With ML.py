edd81a7aaefdaf73d22ba92eb3b8ef90700d2766
Yusuf Al-Khafaji <GLBcraft1234@gmail.com>
2024-03-13 07:47:56 +0100

    prediction with tabs (test if it works properly vs old version)

--- pages/Prediction With ML.py
+++ pages/Prediction With ML.py
@@ -17,7 +17,7 @@ st.set_page_config(
 )
 
 
-
+df = st.session_state['df']
 
 
 st.write("")
@@ -26,8 +26,6 @@ classification = None
 kmeans = None
 dfCluster = st.session_state['dfNumeric'].drop(['gdp_per_capita'], axis=1)
 dfClassification = st.session_state['dfNumeric'].copy()
-
-df = datacleaner.load_data("./data/data_science_salaries.csv")
 
 try:
     st.warning("If this is the first time you are running this app, it may take a while to load the models as they will be trained. Please be patient.")
@@ -88,57 +86,79 @@ with tab2:
     if st.button("Fix NS_ERROR_FAILURE or a white box below"): # This is a workaround for a bug in Streamlit. I can't figure out why the error appears, but reloading the data fixes it
         dfCluster = dfCluster.sample(frac=1).reset_index(drop=True)
         st.write("Data has been randomized")
-    template = dfCluster.iloc[:1].copy()
 
 
    
    # Dropdown for Job Title
-    job_title_input = st.selectbox("Job Title", df['job_title'].unique())
+    job_title_input = st.selectbox("Job Title", df['job_title'].unique() )
 
     # Dropdown for Experience Level
-    experience_level_input = st.selectbox("Experience Level", df['experience_level'].unique())
+    experience_level_input = st.selectbox("Experience Level", df['experience_level'].unique()) # Make the order alphabetical
+
+    # Dropdown for salaryUSD
+    salaryUSD_input = st.number_input("salaryUSD", min_value=1, max_value=500000, key='salary_in_usd')
+
+    # Dropdown for employment type
+    employment_type_input = st.selectbox("Employment Type", df['employment_type'].unique())
+
+    # Dropdown for Work Model
+    work_model_input = st.selectbox("Work Model", df['work_models'].unique())
 
     # Dropdown for Company Location
     company_location_input = st.selectbox("Company Location", df['company_location'].unique())
 
-    # Dropdown for Work Model
-    work_model_input = st.selectbox("Work Model", df['work_models'].unique())
     # Dropdown for Work Year
     work_year_input = st.selectbox("Work Year", df['work_year'].unique())
+
+    # Dropdown for Company Size
+    company_size_input = st.selectbox("Company Size", df['company_size'].unique())
    
 
 
-    def createNewRow(job_title, experience_level, company_location, work_model, work_year):
-        inputs = {}
-        values = [job_title, experience_level, company_location, work_model, work_year]
+    def createNewRow(job_title, experience_level, company_location, work_model, work_year, salaryUSD, employment_type, company_size):
 
-        prefixes = ['job_title_', 'experience_level_', 'company_location_', 'work_models_', 'work_year_']
-        
-        # Iterate through prefixes and input values to create the input dictionary
-        for prefix, value in zip(prefixes, values):
-            column_name = f"{prefix}{value}"
-            if column_name in dfCluster.columns:
-                inputs[column_name] = 1
-                # Set all other columns with the same prefix to 0
-                for col in dfCluster.columns:
-                    if col.startswith(prefix) and col != column_name:
-                        inputs[col] = 0
+            inputs = {}
 
-        # Convert the dictionary to a Series and then to a DataFrame
-        input_row = pd.Series(inputs)
-        input_row = pd.DataFrame([input_row])
+            # Directly assign values for columns without prefixes
+            direct_columns = ['work_year', 'salary_in_usd']
+            direct_values = [work_year, salaryUSD]
 
-        # Ensure the input_row has the same columns as dfCluster
-        input_row = input_row.reindex(columns=dfCluster.columns, fill_value=0)
+            for direct_column, direct_value in zip(direct_columns, direct_values):
+                inputs[direct_column] = direct_value
 
-        return input_row
+            prefixes = ['job_title_', 'experience_level_', 'company_location_', 'work_models_', 'employment_type_', 'company_size_']
+
+            # Iterate through prefixes and input values to create the input dictionary
+            for prefix, value in zip(prefixes, [job_title, experience_level, company_location, work_model, employment_type, company_size]):
+                column_name = f"{prefix}{value}"
+                if column_name in dfCluster.columns:
+                    inputs[column_name] = 1
+                    # Set all other columns with the same prefix to 0
+                    for col in dfCluster.columns:
+                        if col.startswith(prefix) and col != column_name:
+                            inputs[col] = 0
+
+            # Convert the dictionary to a Series and then to a DataFrame
+            input_row = pd.Series(inputs)
+            input_row = pd.DataFrame([input_row])
+
+            # Ensure the input_row has the same columns as dfCluster
+            input_row = input_row.reindex(columns=dfCluster.columns, fill_value=0)
+
+            st.write(input_row)
+
+            return input_row
+
 
 
     X = dfCluster.copy()
 
     if (st.button("Predict cluster")):
-        prediction = createNewRow(job_title_input, experience_level_input, company_location_input, work_model_input, work_year_input)
+        prediction = createNewRow(job_title_input, experience_level_input, company_location_input, work_model_input,
+                                   work_year_input, salaryUSD_input, employment_type_input, company_size_input)
+        
         st.write("The predicted cluster for the selected data is: " + kmeans.predict(prediction).__str__())
+        # clusters = pd.DataFrame('Cluster': kmeans) something like this to combine the clusters with the original df.
 
     st.title("KMeans Clustering Analysis")
 
